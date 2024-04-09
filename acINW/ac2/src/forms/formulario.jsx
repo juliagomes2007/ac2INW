a32b81033b4f5f1fc0e7f4cdd7aa27c66a37efc6
Eduardo Viudes <duduzinhochorro@gmail.com>
2024-04-08 23:24:05 -0300

    commit do Viudes

--- acINW/ac2/src/forms/formulario.jsx
+++ acINW/ac2/src/forms/formulario.jsx
@@ -3,9 +3,9 @@ import styles from './forms.module.css'
 function Forms(){
     return(
         <div className={styles.local}>
-        <input className={styles.componente} type="text" placeholder="Nome" required=""></input>
-        <input className={styles.componente} type="email" placeholder="Email" required=""></input>
-        <textarea className={styles.componente} placeholder="Mensagem" required=""></textarea>
+        <input className={styles.componente} type="text" placeholder="Nome"></input>
+        <input className={styles.componente} type="email" placeholder="Email"></input>
+        <textarea className={styles.componente} placeholder="Mensagem"></textarea>
         <button className={styles.botao} type="submit">Enviar</button>
         </div>
     )
