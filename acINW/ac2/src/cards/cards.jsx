81a63058f430649d1af5db21951ca30672d41fde
Eduardo Viudes <duduzinhochorro@gmail.com>
2024-04-08 23:36:22 -0300

    commit do Pedro

--- acINW/ac2/src/cards/cards.jsx
+++ acINW/ac2/src/cards/cards.jsx
@@ -5,17 +5,17 @@ function Cards(){
         <div className={styles.local}>
         <div className={styles.card}>
             <h3 >Julia Leal</h3>
-            <p>Backend</p>
+            <p>Sabe codar em Java e a melhor aluna de Backend</p>
         </div>
 
         <div className={styles.card}>
             <h3>Eduardo Viudes</h3>
-            <p>Frontend</p>
+            <p>Faz de tudo um pouco, mas esta fazendo cursos da alura para melhorar</p>
         </div>
 
         <div className={styles.card}>
             <h3>Pedro Araújo</h3>
-            <p>Design</p>
+            <p>Criador do Bobert, um futuro profissional da area de mecatronica</p>
         </div>
         </div>
     )
